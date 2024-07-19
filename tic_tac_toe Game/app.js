5deb1f5af91c3c95f367971f4705fe11b0188933
Shehzad Iqbal <148006204+codebyshehzad@users.noreply.github.com>
2024-07-19 17:41:41 +0500

    Add files via upload

---
+++ tic_tac_toe Game/app.js
@@ -0,0 +1,77 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn = document.querySelector("#resetBtn");
+let msgContainer = document.querySelector(".msgContainer");
+let newBtn = document.querySelector("#newBtn");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+const resetGame = ()=>
+    {
+      let  turnO = true;
+      enableboxes();
+     msgContainer.classList.add("hide");
+    }
+
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+            box.disabled = true;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+            box.disabled = true;
+        }
+        checkWinner();
+    });
+});
+
+const enableboxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+
+const disableboxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+    const showWinner = (winner) => {
+     msg.innerText = `Conratulation,Winner is ${winner}`;
+     msgContainer.classList.remove("hide");
+     disableboxes();
+};
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+          let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
+                console.log("winner", pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+}
+
+newBtn.addEventListener("click" , resetGame);
+resetBtn.addEventListener("click", resetGame);
